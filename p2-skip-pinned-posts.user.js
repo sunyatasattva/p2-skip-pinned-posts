94140194e702e04fa189ff31c740d4ac930f8849
Niels Lange <info@nielslange.de>
2021-12-20 09:09:45 +0700

    Add icon and apply WordPress formatting

--- p2-skip-pinned-posts.user.js
+++ p2-skip-pinned-posts.user.js
@@ -9,52 +9,70 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-  'use strict';
+( function () {
+	'use strict';
 
-  if ( !document.body.classList.contains("a8c-p2") ) return;
+	if ( ! document.body.classList.contains( 'a8c-p2' ) ) return;
 
-  function stringToElement(html) {
-    const $template = document.createElement('template');
+	function stringToElement( html ) {
+		const $template = document.createElement( 'template' );
 
-    $template.innerHTML = html.trim();
+		$template.innerHTML = html.trim();
 
-    return $template.content.firstChild;
-  }
+		return $template.content.firstChild;
+	}
 
-  const $ = document.querySelector.bind(document);
-  const $$ = document.querySelectorAll.bind(document);
+	const $ = document.querySelector.bind( document );
+	const $$ = document.querySelectorAll.bind( document );
 
-  const isCompactView = Array.from(document.body.classList)
-    .filter( className => className.includes("compact-view") )
-    .length > 0;
-  const bookmarkHTML = `
-  <li class="p2020-bookmarks__item">
-    <a href="#" class="p2020-bookmarks__item-link">
-      <span class="p2020-bookmarks__item-label">Skip Pinned Posts</span>
-    </a>
-  </li>
-  `;
-  const $newBookmark = stringToElement(bookmarkHTML);
-  const $bookmarksItems = $("#sidebar nav ul[class*='bookmarks__items']");
+	const isCompactView =
+		Array.from( document.body.classList ).filter( ( className ) =>
+			className.includes( 'compact-view' )
+		).length > 0;
+	const bookmarkHTML = `
+    <li class="p2020-bookmarks__item p2020-bookmarks__skip-pinned">
+      <a href="#" class="p2020-bookmarks__item-link">
+        <span class="p2020-bookmarks__item-label">Skip Pinned Posts</span>
+      </a>
+    </li>
+    `;
+	const $newBookmark = stringToElement( bookmarkHTML );
+	const $bookmarksItems = $( "#sidebar nav ul[class*='bookmarks__items']" );
 
-  $newBookmark.addEventListener("click", (e) => {
-    e.preventDefault();
+	$newBookmark.addEventListener( 'click', ( e ) => {
+		e.preventDefault();
 
-    const $firstNonPinnedPost = isCompactView ?
-      Array.from( $$("[data-tippy-content='Pinned post']") )
-        .pop()
-        .closest("article.post")
-        .nextElementSibling
-      : $("article.post:not(.sticky)");
+		const $firstNonPinnedPost = isCompactView
+			? Array.from( $$( "[data-tippy-content='Pinned post']" ) )
+					.pop()
+					.closest( 'article.post' ).nextElementSibling
+			: $( 'article.post:not(.sticky)' );
 
-    $firstNonPinnedPost.scrollIntoView({ behavior: "smooth" });
-  });
+		$firstNonPinnedPost.scrollIntoView( { behavior: 'smooth' } );
+	} );
 
-  if (!$bookmarksItems) {
-      console.warn("P2 Skip Pinned Post doesn't support this P2 theme");
-      return;
-  }
+	if ( ! $bookmarksItems ) {
+		console.warn( "P2 Skip Pinned Post doesn't support this P2 theme" );
+		return;
+	}
 
-  $bookmarksItems.appendChild($newBookmark);
-})();
+	$bookmarksItems.appendChild( $newBookmark );
+
+	const $styles = `
+    .p2020-bookmarks__item.p2020-bookmarks__skip-pinned a:before {
+      content: "";
+      display: block;
+      -webkit-mask: url(/wp-content/themes/pub/p2020/img/icons/custom-chevron.svg) no-repeat;
+      mask: url(/wp-content/themes/pub/p2020/img/icons/custom-chevron.svg) no-repeat;
+      -webkit-mask-size: cover;
+      mask-size: cover;
+      width: 1.5rem;
+      height: 1.5rem;
+      background: var(--p2-color-link);
+    }
+    `;
+
+	const $styleSheet = document.createElement( 'style' );
+	$styleSheet.innerText = $styles;
+	document.head.appendChild( $styleSheet );
+} )();
